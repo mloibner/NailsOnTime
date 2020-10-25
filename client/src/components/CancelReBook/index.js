5396d4b3921ed9e97f497eb2af3416c00519273d
Monique Loibner <m.loibner@hotmail.com>
2020-10-25 22:49:12 +1100

    member page with booking active completed for button functionality and router requests.

--- client/src/components/CancelReBook/index.js
+++ client/src/components/CancelReBook/index.js
@@ -1,22 +1,33 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import "../style.css";
-import { useHistory } from "react-router-dom"
-import axios from "axios"
+import { useHistory } from "react-router-dom";
+import axios from "axios";
 
 function CancelReBook() {
     const history = useHistory();
-    const [booking, deleteBooking] = useState({booked:""})
+    const [booking, deleteBooking] = useState({booked:""});
+    const [updatedBooking, changeBooking] = useState("")
 
     function newDelete(){
-        axios.delete("/api/booking")
+        axios.delete("/api/booking/delete", booking).then(()=>{
+            history.push("/member");
+        });
+    };
 
+    function bookingChange(){
+        axios.put("/api/booking/change").then(()=>{
+            history.push("/booking")
+        })
     }
 
+    useEffect(()=>{
+        newDelete();
+        bookingChange();
+    });
+
    return ( <div className="homeDiv">
-        <button className="btn" onClick={()=>{
-            history.push("/member")
-        }}>Cancel</button>
-        <button className="btn" onClick>Change</button>
+        <button className="btn" onClick={deleteBooking}>Cancel</button>
+        <button className="btn" onClick={changeBooking}>Change</button>
     </div>)
 };
 
